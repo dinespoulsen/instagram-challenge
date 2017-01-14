bb05e6a59700a99f6321fa25d39451e48c71a980
Dines Poulsen <dinespoulsen84@gmail.com>
2017-01-14 10:38:18 +0000

    Can add a picture with title and description

--- instagram/app/controllers/pictures_controller.rb
+++ instagram/app/controllers/pictures_controller.rb
@@ -1,7 +1,20 @@
 class PicturesController < ApplicationController
 
   def index
-    puts "hello"
+    @pictures = Picture.all
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    Picture.create(picture_params)
+    redirect_to pictures_path
+  end
+
+  def picture_params
+    params.require(:picture).permit(:title, :description)
   end
 
 end
--- instagram/spec/features/pictures_feature_spec.rb
+++ instagram/spec/features/pictures_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'pictures' do
       expect(page).to have_link 'Add a picture'
     end
   end
+
+  context 'when uploading a picture' do
+    scenario "can add a picture" do
+      visit('/pictures')
+      click_link('Add a picture')
+      fill_in("picture[title]", with: "Isabel")
+      fill_in("picture[description]", with: "picture of Isabel")
+      # attach_file("image", Rails.root + "spec/fixtures/file.pdf")
+      click_button("Add picture")
+      expect(page).to have_content("Isabel")
+    end
+  end
 end
