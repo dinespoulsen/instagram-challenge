9ea4e3b40033c86b5637d11420eec4fb13baeaa7
Dines Poulsen <dinespoulsen84@gmail.com>
2017-01-16 14:25:31 +0000

    photo upload to amazon web service

--- instagram/config/environments/development.rb
+++ instagram/config/environments/development.rb
@@ -1,4 +1,20 @@
 Rails.application.configure do
+  PAPERCLIP_STORAGE_OPTIONS = {:storage => :s3,
+                               :s3_credentials => "#{Rails.root}/config/s3.yml",
+                               :path => "/:style/:filename"}
+
+    config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    },
+    url: 'instagram-clone-challenge.s3.amazonaws.com',
+    path: '/:class/:attachment/:id_partition/:style/:filename',
+    s3_host_name: 's3-eu-west-2.amazonaws.com',
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
